7792102ab3387f06e8905fb08ab266dd7b1926f8
shakkeerz <shakkeerpersonal@gmail.com>
2023-12-13 14:31:51 +0530

    cd.yml updates to main

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,4 @@
-
-type Props = {};
-
- export const Error = (props: Props) => {
+ export const Error = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
